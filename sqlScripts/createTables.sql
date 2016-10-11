614f54af7efb34ad2335dee4d36d96f45fa31e99
Ijaaz <ijaazlagardien@gmail.com>
2016-10-11 21:27:04 +0200

    updated table scripts

--- sqlScripts/createTables.sql
+++ sqlScripts/createTables.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2016 at 06:34 PM
+-- Generation Time: Oct 11, 2016 at 07:23 PM
 -- Server version: 5.7.9
 -- PHP Version: 5.6.16
 
@@ -29,11 +29,12 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `brands`;
 CREATE TABLE IF NOT EXISTS `brands` (
   `BrandID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` text NOT NULL,
+  `Name` varchar(25) NOT NULL,
   `SuplierID` int(11) NOT NULL,
-  `SupplierName` text NOT NULL,
-  PRIMARY KEY (`BrandID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `SupplierName` varchar(25) NOT NULL,
+  PRIMARY KEY (`BrandID`),
+  KEY `SuplierID` (`SuplierID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -45,11 +46,12 @@ DROP TABLE IF EXISTS `customers`;
 CREATE TABLE IF NOT EXISTS `customers` (
   `customerID` int(11) NOT NULL AUTO_INCREMENT,
   `userID` int(11) NOT NULL,
-  `CustomerName` text NOT NULL,
-  `CustomerSurname` text NOT NULL,
-  `ContactNumber` text NOT NULL,
-  PRIMARY KEY (`customerID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `CustomerName` varchar(25) NOT NULL,
+  `CustomerSurname` varchar(25) NOT NULL,
+  `ContactNumber` varchar(25) NOT NULL,
+  PRIMARY KEY (`customerID`),
+  KEY `userID` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -61,15 +63,17 @@ DROP TABLE IF EXISTS `deliveries`;
 CREATE TABLE IF NOT EXISTS `deliveries` (
   `DeliveryID` int(11) NOT NULL AUTO_INCREMENT,
   `distributorID` int(11) NOT NULL,
-  `courierName` text NOT NULL,
+  `courierName` varchar(25) NOT NULL,
   `CustomerID` int(11) NOT NULL,
-  `RecipientName` text NOT NULL,
-  `DeliveryAddress` text NOT NULL,
+  `RecipientName` varchar(25) NOT NULL,
+  `DeliveryAddress` varchar(25) NOT NULL,
   `DateDespatched` date NOT NULL,
   `DateDeliveried` date NOT NULL,
   `totalItems` int(11) NOT NULL,
-  PRIMARY KEY (`DeliveryID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`DeliveryID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `distributorID` (`distributorID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -80,10 +84,10 @@ CREATE TABLE IF NOT EXISTS `deliveries` (
 DROP TABLE IF EXISTS `departments`;
 CREATE TABLE IF NOT EXISTS `departments` (
   `departmentID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` text NOT NULL,
+  `Name` varchar(25) NOT NULL,
   `totalEmployees` int(11) NOT NULL,
   PRIMARY KEY (`departmentID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -94,12 +98,12 @@ CREATE TABLE IF NOT EXISTS `departments` (
 DROP TABLE IF EXISTS `disributors`;
 CREATE TABLE IF NOT EXISTS `disributors` (
   `distributorID` int(11) NOT NULL AUTO_INCREMENT,
-  `companyName` text NOT NULL,
-  `contactNumber` text NOT NULL,
-  `emailAddress` text NOT NULL,
-  `physicalAddress` text NOT NULL,
+  `companyName` varchar(25) NOT NULL,
+  `contactNumber` varchar(25) NOT NULL,
+  `emailAddress` varchar(25) NOT NULL,
+  `physicalAddress` varchar(125) NOT NULL,
   PRIMARY KEY (`distributorID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -110,13 +114,14 @@ CREATE TABLE IF NOT EXISTS `disributors` (
 DROP TABLE IF EXISTS `employees`;
 CREATE TABLE IF NOT EXISTS `employees` (
   `employeeID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` text NOT NULL,
-  `Surname` text NOT NULL,
-  `Role` text NOT NULL,
+  `Name` varchar(25) NOT NULL,
+  `Surname` varchar(25) NOT NULL,
+  `Role` varchar(25) NOT NULL,
   `DepartmentID` int(11) NOT NULL,
-  `Department` text NOT NULL,
-  PRIMARY KEY (`employeeID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Department` varchar(25) NOT NULL,
+  PRIMARY KEY (`employeeID`),
+  KEY `DepartmentID` (`DepartmentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -129,12 +134,14 @@ CREATE TABLE IF NOT EXISTS `orderhistory` (
   `OrderID` int(11) NOT NULL AUTO_INCREMENT,
   `CustomerID` int(11) NOT NULL,
   `ProductID` int(11) NOT NULL,
-  `CustomerName` text NOT NULL,
-  `quantity` int(11) NOT NULL,
+  `CustomerName` varchar(25) NOT NULL,
+  `Quantity` int(11) NOT NULL,
   `Date` date NOT NULL,
   `TotalCost` double NOT NULL,
-  PRIMARY KEY (`OrderID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`OrderID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `ProductID` (`ProductID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -147,14 +154,16 @@ CREATE TABLE IF NOT EXISTS `products` (
   `productID` int(11) NOT NULL AUTO_INCREMENT,
   `brandID` int(11) NOT NULL,
   `SupplierID` int(11) NOT NULL,
-  `Brand` text NOT NULL,
-  `Supplier` text NOT NULL,
+  `Brand` varchar(25) NOT NULL,
+  `Supplier` varchar(25) NOT NULL,
   `Quantity` int(11) NOT NULL,
   `price` double NOT NULL,
   `instock` tinyint(1) NOT NULL,
   `image` longblob NOT NULL,
-  PRIMARY KEY (`productID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`productID`),
+  KEY `brandID` (`brandID`),
+  KEY `SupplierID` (`SupplierID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -166,11 +175,12 @@ DROP TABLE IF EXISTS `shoppingcart`;
 CREATE TABLE IF NOT EXISTS `shoppingcart` (
   `itemID` int(11) NOT NULL AUTO_INCREMENT,
   `productID` int(11) NOT NULL,
-  `productName` text NOT NULL,
+  `productName` varchar(25) NOT NULL,
   `price` double NOT NULL,
   `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`itemID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`itemID`),
+  KEY `productID` (`productID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -181,12 +191,12 @@ CREATE TABLE IF NOT EXISTS `shoppingcart` (
 DROP TABLE IF EXISTS `suppliers`;
 CREATE TABLE IF NOT EXISTS `suppliers` (
   `supplierID` int(11) NOT NULL AUTO_INCREMENT,
-  `SupplierName` text NOT NULL,
-  `ContactNumber` text NOT NULL,
-  `EmailAddress` text NOT NULL,
-  `physicalAddress` text NOT NULL,
+  `SupplierName` varchar(25) NOT NULL,
+  `ContactNumber` varchar(25) NOT NULL,
+  `EmailAddress` varchar(25) NOT NULL,
+  `physicalAddress` varchar(125) NOT NULL,
   PRIMARY KEY (`supplierID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -197,11 +207,60 @@ CREATE TABLE IF NOT EXISTS `suppliers` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `userID` int(11) NOT NULL AUTO_INCREMENT,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  `accountType` text NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `accountType` varchar(25) NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `brands`
+--
+ALTER TABLE `brands`
+  ADD CONSTRAINT `brands_ibfk_1` FOREIGN KEY (`SuplierID`) REFERENCES `suppliers` (`supplierID`);
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+
+--
+-- Constraints for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  ADD CONSTRAINT `deliveries_ibfk_1` FOREIGN KEY (`distributorID`) REFERENCES `disributors` (`distributorID`),
+  ADD CONSTRAINT `deliveries_ibfk_2` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`customerID`);
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`departmentID`);
+
+--
+-- Constraints for table `orderhistory`
+--
+ALTER TABLE `orderhistory`
+  ADD CONSTRAINT `orderhistory_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`customerID`),
+  ADD CONSTRAINT `orderhistory_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`productID`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brandID`) REFERENCES `brands` (`BrandID`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`SupplierID`) REFERENCES `suppliers` (`supplierID`);
+
+--
+-- Constraints for table `shoppingcart`
+--
+ALTER TABLE `shoppingcart`
+  ADD CONSTRAINT `shoppingcart_ibfk_1` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
